0cfff7dfc2b373676aac4b6db36e405784e57854
seojeongkuk <wjdrnr6662@gmail.com>
2022-08-10 22:08:27 +0900

    index 파일 수정

--- ts-todo/start/src/index.ts
+++ ts-todo/start/src/index.ts
@@ -1,1 +1,38 @@
-console.log('hello world');
+import {waitForInput} from "./Input";
+import {AppState, Priority} from "./type";
+import Todo from "./Todo";
+import {Command, CommandPrintTodos} from "./Command";
+
+const commands: Command[] = [
+    new CommandPrintTodos()
+]
+
+async function main() {
+    const state: AppState = {
+        todos: [
+            new Todo('test1', Priority.High),
+            new Todo('test2', Priority.Medium),
+            new Todo('test3', Priority.Low)
+        ]
+    }
+
+    while(true) {
+        console.clear();
+        for(const command of commands) {
+            console.log(command.toString())
+        }
+        console.log();
+
+        const key = await waitForInput('input command:')
+        console.clear();
+
+        const command = commands.find(item => item.key === key)
+        if (command) {
+            await command.run(state);
+        }
+
+        console.log('key', key)
+    }
+}
+
+main();
